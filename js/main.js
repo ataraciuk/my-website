4667b0e2cf32cf3c9e7e6b2fe22c87c9102164bc
ataraciuk <ataraciuk@gmail.com>
2014-04-19 23:05:02 -0400

    different cursor and resume goes to a file

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ andrest.init = function() {
 	andrest.window = $(window);
 	andrest.email = 'contact@andrest.me';
 	$('.myEmail').attr({href: 'mailto:'+andrest.email}).html(andrest.email);
+	window.onhashchange = function(){
+		var section = location.hash;
+	};
 }
 
 $(function() {
